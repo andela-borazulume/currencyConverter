65e41a00b8c71ee45607591842a7b9cf3a6fc298
Blessing Orazulume <blessing.orazulume@andela.com>
2016-07-19 13:19:02 +0100

    Completed the GUI for the currency converter

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,3 +1,7 @@
+import GUI.CalculatorLayout;
+
+import javax.swing.*;
+
 /**
  * Created by blessingorazulume on 7/14/16.
  */
@@ -7,5 +11,10 @@ public class App {
 
     public static void main(String[] args){
         //we want to build our project here
+        CalculatorLayout layout = new CalculatorLayout();
+        layout.setSize(300, 500);
+        layout.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        layout.setVisible(true);
+        layout.setResizable(false);
     }
 }
